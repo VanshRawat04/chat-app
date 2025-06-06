c2e027a4cc62842148327e317c11e35b13152f2d
VANSH RAWAT <vanshrawat@VANSHs-MacBook-Pro.local>
2025-06-07 00:19:02 +0530

    vercel config

--- server/server.js
+++ server/server.js
@@ -49,5 +49,11 @@ app.use('/api/message', messageRouter )
 //connect to mongoDB
 await connectDB()
 
-const PORT = process.env.PORT || 5001
-server.listen(PORT, ()=> console.log('server is running on PORT: ' + PORT))
+
+if(process.env.NODE_ENV !== 'production'){
+    const PORT = process.env.PORT || 5001
+    server.listen(PORT, ()=> console.log('server is running on PORT: ' + PORT))
+}
+
+//export server for vercel
+export default server
